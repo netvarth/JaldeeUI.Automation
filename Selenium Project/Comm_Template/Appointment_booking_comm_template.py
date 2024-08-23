b2140c2b8a54ea5ba9dd67e529eb1de5bc93bd51
Sanoop A S <sanoop.as@jaldee.com>
2024-08-23 17:37:26 +0530

    Comm_template 23082024

--- Selenium Project/Comm_Template/Appointment_booking_comm_template.py
+++ Selenium Project/Comm_Template/Appointment_booking_comm_template.py
@@ -1,79 +1,291 @@
+import uuid
+import allure
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
 from Framework.common_utils import *
 
-
 @allure.severity(allure.severity_level.CRITICAL)
-@allure.title("Create comfirmation template")
+@allure.title("Create confirmation template")
 @pytest.mark.parametrize('url', ["https://scale.jaldee.com/business/"])
 def test_appt_confirmation(login):
-
-    time.sleep(5)
+    
     WebDriverWait(login, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//div[contains(text(),'Settings')]"))
+        EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Settings')]"))
     ).click()
 
-    time.sleep(2)
-    notification_option = WebDriverWait(login, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//div[normalize-space()='Communications And Notifications']"))
-    )
-    login.execute_script("arguments[0].scrollIntoView();", notification_option)
-
-    time.sleep(2)
     WebDriverWait(login, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//li//a//span[@class='lnk setings ml-auto' and normalize-space(text())='Notifications']"))
+        EC.presence_of_element_located((By.XPATH, "//div[normalize-space()='Communications And Notifications']"))
     ).click()
 
-    time.sleep(2)
     WebDriverWait(login, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//h2[normalize-space()='Custom Templates']"))
+        EC.presence_of_element_located((By.XPATH, "//li//a//span[@class='lnk setings ml-auto' and normalize-space(text())='Notifications']"))
     ).click()
 
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//h2[normalize-space()='Custom Templates']"))
+    ).click()
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='Create New']"))
+    ).click()
     time.sleep(2)
     WebDriverWait(login, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//span[normalize-space()='Create New']"))
+        EC.presence_of_element_located((By.XPATH, "//p-dropdown[@optionlabel='context']//div[@aria-label='dropdown trigger']"))
     ).click()
 
-    time.sleep(5)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='APPOINTMENT']"))
+    ).click()
+
     WebDriverWait(login, 10).until(
         EC.presence_of_element_located(
-            (By.XPATH, "//p-dropdown[@optionlabel='context']//div[@aria-label='dropdown trigger']"))
+            (By.XPATH, "(//div[@aria-label='dropdown trigger'])[2]"))
     ).click()
-
-    time.sleep(2)
+    time.sleep(1)
     WebDriverWait(login, 10).until(
         EC.presence_of_element_located(
-            (By.XPATH, "//span[normalize-space()='APPOINTMENT']"))
+            (By.XPATH, "//span[@class='ng-star-inserted'][normalize-space()='Appointment Confirmation']"))
     ).click()
 
+    time.sleep(3)
+    temp_name = "appt_confirmation" + str(uuid.uuid4())[:4]
     template_namebox = WebDriverWait(login, 10).until(
         EC.presence_of_element_located((By.XPATH, "//input[@autocomplete='off']"))
     )
-
-    time.sleep(2)
-    temp_name = "appt_confirmation" + str(uuid.uuid4())[:4]
-    template_namebox.click()
-
-    WebDriverWait(login, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//div[@class='form-group']//input[contains(@class, 'p-inputtext')]"))
-    ).send_keys(temp_name)
+    template_namebox.clear()
+    template_namebox.send_keys(temp_name)
 
     login.find_element(By.XPATH, "//span[normalize-space()='Whatsapp']").click()
     login.find_element(By.XPATH, "//span[normalize-space()='Email']").click()
 
     WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='Save & Next']"))
+    ).click()
+
+    editer1= WebDriverWait(login, 10).until(
         EC.presence_of_element_located(
-            (By.XPATH, "//span[normalize-space()='Edit Saved Template']"))
+            (By.XPATH, "//div[@aria-label='Rich Text Editor. Editing area: main']"))
+    )
+    editer1.send_keys("Hello")
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//div[@class='pull-right tag-style mgn-lt-5' and contains(img/@src, 'VariableTag.png')]"))
     ).click()
     
+    Consumer_name = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//li[@aria-label='Consumer name']")) 
+    )
+    Consumer_name.click()
 
+    editer1.click()
+    editer1.send_keys(Keys.ENTER)
+    editer1.send_keys("Booking for")
+
+    time.sleep(2)
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//div[@class='pull-right tag-style mgn-lt-5' and contains(img/@src, 'VariableTag.png')]"))
+    ).click()
+
+    business_name = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//li[@aria-label='Business Name']"))
+    )
+    business_name.click()
+
+    editer1.click()
+    editer1.send_keys(Keys.ENTER)
+
+    time.sleep(2)
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//div[@class='pull-right tag-style mgn-lt-5' and contains(img/@src, 'VariableTag.png')]"))
+    ).click()
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//li[@aria-label='User name']"))
+    ).click()
+
+    editer1.click()
+    editer1.send_keys(Keys.ENTER)
+
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//div[@class='actiondiv mgn-lt-10 desktop-only']//button[@class='cs-btn btn btn-primary btn-transform']"))
+    ).click()
+
+    time.sleep(2)
+    ripple_switch = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "(//span[contains(text(),'Inactive')])[1]"))
+    )
+    ripple_switch.click()
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//img[@src='./assets/images/menu/home-color.png']"))
+    ).click()
+
+    WebDriverWait(login, 20).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//div[contains(@class, 'font-small') and contains(text(),'Appointments')]",
+            )
+        )
+    ).click()
+    time.sleep(3)
+    element = WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//div[contains(@class, 'my-1') and .//span[text()='Appointment']]",
+            )
+        )
+    )
+    element.click()
+    time.sleep(3)
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//input[@placeholder='Enter name or phone or id']")
+        )
+    ).send_keys("920720600")
+    time.sleep(2)
+    login.implicitly_wait(5)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Id : 2']")
+        )
+    ).click()
+
+    login.implicitly_wait(3)
+    WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "p-dropdown[optionlabel='place']")
+        )
+    ).click()
+
+    login.implicitly_wait(5)
+    login.find_element(By.XPATH, "(//li[@id='p-highlighted-option'])[1]").click()
+    print("location : Chavakkad")
+    login.implicitly_wait(5)
+
+    login.find_element(
+        By.CSS_SELECTOR, "p-dropdown[optionlabel='departmentName']"
+    ).click()
+    login.implicitly_wait(5)
+    login.find_element(By.XPATH, "(//li[@aria-label='ENT'])[1]").click()
+    print("Department : ENT")
+    user_dropdown_xpath = (
+        "(//p-dropdown[@class='p-element p-inputwrapper p-inputwrapper-filled ng-untouched ng-valid "
+        "ng-dirty'])[1]"
+    )
+    WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, user_dropdown_xpath))
+    ).click()
+    user_option_xpath = "(//li[@aria-label='Naveen KP'])[1]"
+    WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, user_option_xpath))
+    ).click()
+    print("Select user : Naveen")
+
+    service_dropdown_xpath = "//p-dropdown[@optionlabel='name']"
+    element = login.find_element(By.XPATH, service_dropdown_xpath)
+    login.execute_script("arguments[0].scrollIntoView();", element)
+    element.click()
+
+    service_option_xpath = (
+        "(//div[@class='option-container ng-star-inserted'][normalize-space()='Naveen "
+        "Consultation'])[2]"
+    )
+    WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, service_option_xpath))
+    ).click()
+    print("Select Service : Naveen Consultation")
     time.sleep(5)
+    Today_Date = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "//span[@class='mat-calendar-body-cell-content mat-focus-indicator mat-calendar-body-selected mat-calendar-body-today']",
+            )
+        )
+    )
+    login.implicitly_wait(10)
+    Today_Date.click()
+    print("Today Date:", Today_Date.text)
+    wait = WebDriverWait(login, 10)
+    time_slot = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@aria-selected='true']"))
+    )
+    time_slot.click()
+    print("Time Slot:", time_slot.text)
 
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Notes')]"))
+    ).click()
 
+    login.find_element(By.XPATH, "//textarea[@id='message']").send_keys(
+        "Note for the walkin appointment"
+    )
 
- 
+    WebDriverWait(login, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Save']")
+        )
+    ).click()
+    print("Note added for walkin appointment")
 
+    time.sleep(3)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Upload File']")
+        )
+    ).click()
 
+    time.sleep(4)
+    # Get the current working directory
+    current_working_directory = os.getcwd()
+
+    # Construct the absolute path
+    absolute_path = os.path.abspath(
+        os.path.join(current_working_directory, r"Extras\test.png")
+    )
+    pyautogui.write(absolute_path)
+    pyautogui.press("enter")
+    print("Successfully upload the file")
+
+    time.sleep(6)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//span[contains(text(),'Confirm')]")
+        )
+    ).click()
+
+    try:
+
+        snack_bar = WebDriverWait(login, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "snackbarnormal"))
+        )
+        message = snack_bar.text
+        print("Snack bar message:", message)
+
+    except:
+
+        snack_bar = WebDriverWait(login, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "snackbarerror"))
+        )
+        message = snack_bar.text
+        print("Snack bar message:", message)
