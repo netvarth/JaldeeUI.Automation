2d6ad75012e5381d2a44bb882a4dcf5d40811d9a
Priya <priya.c@jaldee.com>
2024-09-13 11:47:41 +0530

    signup

--- Selenium Project/Deployment/Post Deployment Consumer_Signup.py
+++ Selenium Project/Deployment/Post Deployment Consumer_Signup.py
@@ -28,6 +28,10 @@ def login():
     yield driver
     driver.quit() 
 
+def generate_random_salutation():
+    salutations = ["Mr.", "Ms.", "Mrs.", "Master", "Miss", "B/o", "Dr.", "Adv.", "Fr."]
+    return random.choice(salutations)
+
 def create_consumer_data():
     fake = Faker()
     first_name = fake.first_name()
@@ -268,5 +272,337 @@ def test_signup_token_booking(login):
     ).click()
     print("New Consumer Appointment booking confirmed successfully")
     time.sleep(3)
+
+
+def test_signup_familymember_appointment_booking(login):
+    consumer_data = create_consumer_data()
+    time.sleep(5)
+    # Scroll to the element
+    book_now_button = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Book Now']")
+        )
+    )
+    login.execute_script("arguments[0].scrollIntoView();", book_now_button)
+
+    # Wait for the element to be clickable
+    clickable_book_now_button = WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='Book Now']"))
+    )
+
+    # Attempt to click the element
+    try:
+        clickable_book_now_button.click()
+    except:
+        # If click is intercepted, click using JavaScript
+        login.execute_script("arguments[0].click();", clickable_book_now_button)
+
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "//app-appointment-card[@class='ng-star-inserted']//div//div[@class='serviceName ng-star-inserted'][normalize-space()='Consultation']",
+            )
+        )
+    ).click()
+    time.sleep(2)
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    Today_Date = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[@aria-pressed='true'] [@aria-current='date']")
+        )
+    )
+    Today_Date.click()
+    time.sleep(2)
+    print("Today Date:", Today_Date.text)
+    wait = WebDriverWait(login, 10)
+    time_slot = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//mat-chip[@aria-selected='true']"))
+    )
+    time_slot.click()
+    print("Time Slot:", time_slot.text)
+    next_button = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[normalize-space()='Next']")
+        )
+    )
+    login.execute_script("arguments[0].scrollIntoView(true);", next_button)
+    time.sleep(3)
+    next_button.click()
+    time.sleep(3)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//input[@id='phone']"))
+    ).send_keys(consumer_data['phonenumber'])
+    print("New Consumer Phone Number:", consumer_data['phonenumber'])
+    login.find_element(By.XPATH, "//span[@class='continue ng-star-inserted']").click()
+    time.sleep(5)
+    otp_inputs = WebDriverWait(login, 10).until(
+        EC.presence_of_all_elements_located(
+            (By.XPATH, "//input[contains(@id, 'otp_')]")
+        )
+    )
+    for i, otp_input in enumerate(otp_inputs):
+        otp_input.send_keys(consumer_data['otp'][i])
+    login.find_element(By.XPATH, "//span[@class='continue ng-star-inserted']").click()
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "(//input[@id='first_name'])[1]"))
+    ).send_keys(consumer_data['first_name'])
+    print("New Consumer Firstname:", consumer_data['first_name'])
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "(//input[@id='first_name'])[2]"))
+    ).send_keys(consumer_data['last_name'])
+    print("New Consumer Lastname:", consumer_data['last_name'])
+    login.find_element(By.XPATH, "//span[normalize-space()='Next']").click()
+    time.sleep(5)
+    add_family_icon = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//i[@class='fa fa-pencil-square-o member']")
+        )
+    )
+    add_family_icon.click()
+    time.sleep(2)
+    add_member = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//a[normalize-space()='Add Member']")
+        )
+    )
+    add_member.click()
+    time.sleep(2)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//p-dropdown[@placeholder='Select']")
+        )
+    ).click()
+    time.sleep(3)
+    salutation = generate_random_salutation()
+    salutation_option_xpath = f"//li[@aria-label='{salutation}']"
+    salutation_option_element = WebDriverWait(login, 15).until(
+        EC.element_to_be_clickable((By.XPATH, salutation_option_xpath))
+    )
+    salutation_option_element.click()
+    time.sleep(3)
+    consumer_data = create_consumer_data()
+    login.find_element(By.XPATH, "//input[@id='first_name']").send_keys(consumer_data['first_name'])
+    print("New Consumer Firstname:", consumer_data['first_name'])
+    login.find_element(By.XPATH, "//input[@id='lastname']").send_keys(consumer_data['last_name'])
+    print("New Consumer Lastname:", consumer_data['last_name'])
+    login.find_element(By.XPATH, "//button[normalize-space()='Ok']").click()
+    time.sleep(3)
+    family_member_radio_button = WebDriverWait(login, 10).until(
+    EC.presence_of_element_located(
+        (By.XPATH, "(//span[@class='mat-radio-label-content'])[2]")
+    )
+    )
+    family_member_radio_button.click()
+    time.sleep(2)
+    print("Selected Familymember:", family_member_radio_button.text)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[normalize-space()='Ok']")
+        )
+    ).click()
+    time.sleep(3)
+    consumer_notes = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//textarea[@placeholder='Add Notes you may have...']")
+        )
+    )
+    consumer_notes.send_keys("Notes added from conumser side")
+    time.sleep(3)
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[@class='uploadFileTxt']"))
+    ).click()
+    time.sleep(2)
+    current_working_directory = os.getcwd()
+    absolute_path = os.path.abspath(
+        os.path.join(current_working_directory, r"Extras\test.png")
+    )
+    pyautogui.write(absolute_path)
+    pyautogui.press("enter")
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    confirmbutton = WebDriverWait(login, 15).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Confirm']")
+        )
+    )
+    confirmbutton.click()
+    time.sleep(5)
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='Ok']"))
+    ).click()
+    print("New Consumer Appointment booking confirmed successfully")
+    time.sleep(3)
+
+
+def test_signup_token_familymember_booking(login):
+    consumer_data = create_consumer_data()
+    time.sleep(5)
+    # Scroll to the element
+    book_now_button = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Book Now']")
+        )
+    )
+    login.execute_script("arguments[0].scrollIntoView();", book_now_button)
+
+    # Wait for the element to be clickable
+    clickable_book_now_button = WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='Book Now']"))
+    )
+
+    # Attempt to click the element
+    try:
+        clickable_book_now_button.click()
+    except:
+        # If click is intercepted, click using JavaScript
+        login.execute_script("arguments[0].click();", clickable_book_now_button)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "//app-checkin-card[@class='ng-star-inserted']//div//div[@class='serviceName ng-star-inserted'][normalize-space()='Consultation']",
+            )
+        )
+    ).click()
+    time.sleep(2)
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    Today_Date = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[@aria-pressed='true'] [@aria-current='date']")
+        )
+    )
+    Today_Date.click()
+    time.sleep(2)
+    print("Today Date:", Today_Date.text)
+    queue = WebDriverWait(login, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//mat-chip[@role='option']")
+        )
+    )
+    login.execute_script("arguments[0].scrollIntoView(true);", queue)
+    time.sleep(2)
+    queue.click()
+    print("Queue:", queue.text)
+    time.sleep(2)
+    next_button = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[normalize-space()='Next']")
+        )
+    )
+    login.execute_script("arguments[0].scrollIntoView(true);", next_button)
+    time.sleep(3)
+    next_button.click()
+    time.sleep(3)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//input[@id='phone']"))
+    ).send_keys(consumer_data['phonenumber'])
+    print("New Consumer Phone Number:", consumer_data['phonenumber'])
+    login.find_element(By.XPATH, "//span[@class='continue ng-star-inserted']").click()
+    time.sleep(5)
+    otp_inputs = WebDriverWait(login, 10).until(
+        EC.presence_of_all_elements_located(
+            (By.XPATH, "//input[contains(@id, 'otp_')]")
+        )
+    )
+    for i, otp_input in enumerate(otp_inputs):
+        otp_input.send_keys(consumer_data['otp'][i])
+    login.find_element(By.XPATH, "//span[@class='continue ng-star-inserted']").click()
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "(//input[@id='first_name'])[1]"))
+    ).send_keys(consumer_data['first_name'])
+    print("New Consumer Firstname:", consumer_data['first_name'])
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "(//input[@id='first_name'])[2]"))
+    ).send_keys(consumer_data['last_name'])
+    print("New Consumer Lastname:", consumer_data['last_name'])
+    login.find_element(By.XPATH, "//span[normalize-space()='Next']").click()
+    time.sleep(5)
+    add_family_icon = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//i[@class='fa fa-pencil-square-o member']")
+        )
+    )
+    add_family_icon.click()
+    time.sleep(2)
+    add_member = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//a[normalize-space()='Add Member']")
+        )
+    )
+    add_member.click()
+    time.sleep(2)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//p-dropdown[@placeholder='Select']")
+        )
+    ).click()
+    time.sleep(3)
+    salutation = generate_random_salutation()
+    salutation_option_xpath = f"//li[@aria-label='{salutation}']"
+    salutation_option_element = WebDriverWait(login, 15).until(
+        EC.element_to_be_clickable((By.XPATH, salutation_option_xpath))
+    )
+    salutation_option_element.click()
+    time.sleep(3)
+    consumer_data = create_consumer_data()
+    login.find_element(By.XPATH, "//input[@id='first_name']").send_keys(consumer_data['first_name'])
+    print("New Consumer Firstname:", consumer_data['first_name'])
+    login.find_element(By.XPATH, "//input[@id='lastname']").send_keys(consumer_data['last_name'])
+    print("New Consumer Lastname:", consumer_data['last_name'])
+    login.find_element(By.XPATH, "//button[normalize-space()='Ok']").click()
+    time.sleep(3)
+    family_member_radio_button = WebDriverWait(login, 10).until(
+    EC.presence_of_element_located(
+        (By.XPATH, "(//span[@class='mat-radio-label-content'])[2]")
+    )
+    )
+    family_member_radio_button.click()
+    time.sleep(2)
+    print("Selected Familymember:", family_member_radio_button.text)
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[normalize-space()='Ok']")
+        )
+    ).click()
+    time.sleep(3)
+    consumer_notes = WebDriverWait(login, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//textarea[@placeholder='Add Notes you may have...']")
+        )
+    )
+    consumer_notes.send_keys("Notes added from conumser side")
+    time.sleep(3)
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    WebDriverWait(login, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[@class='uploadFileTxt']"))
+    ).click()
+    time.sleep(2)
+    current_working_directory = os.getcwd()
+    absolute_path = os.path.abspath(
+        os.path.join(current_working_directory, r"Extras\test.png")
+    )
+    pyautogui.write(absolute_path)
+    pyautogui.press("enter")
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    confirmbutton = WebDriverWait(login, 15).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//span[normalize-space()='Confirm']")
+        )
+    )
+    confirmbutton.click()
+    time.sleep(5)
+    login.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    WebDriverWait(login, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='Ok']"))
+    ).click()
+    print("New Consumer token booking confirmed successfully")
+    time.sleep(3)
+
 
 
