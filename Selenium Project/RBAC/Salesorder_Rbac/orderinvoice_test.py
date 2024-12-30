e93d8e2ffa040e952e75a83699c807bbcaff0281
Priya <priya.c@jaldee.com>
2024-12-30 11:31:50 +0530

    orderinvoice

--- Selenium Project/RBAC/Salesorder_Rbac/orderinvoice_test.py
+++ Selenium Project/RBAC/Salesorder_Rbac/orderinvoice_test.py
@@ -1,3 +1,4 @@
+
 from Framework.common_utils import *
 
 
@@ -34,10 +35,10 @@ def test_Rbac_Disabled_createorderInvoice(login):
         print("Checkbox is selected:", is_checked)
         if is_checked:
             print("Checkbox is selected, proceeding to disabled the create order invoice ..")
-            create_order = WebDriverWait(login, 10).until(
+            create_order_invoice = WebDriverWait(login, 10).until(
                 EC.presence_of_element_located((By.XPATH, "(//div[@class='mdc-form-field'])[12]"))
             )
-            create_order.click()
+            create_order_invoice.click()
             time.sleep(2)
             wait_and_locate_click(login, By.XPATH, "//button[normalize-space()='Save']")
             time.sleep(2)
@@ -118,14 +119,11 @@ def test_Rbac_Disabled_createorderInvoice(login):
           attachment_type=AttachmentType.PNG,
           )
           raise e
-     
-
-
 
 @allure.severity(allure.severity_level.CRITICAL)
-@allure.title("RBAC Jaldee Order POS with enabling Create Order_Invoice")
+@allure.title("RBAC Jaldee Order POS with enabling Create Salesorder invoice")
 @pytest.mark.parametrize("url", ["https://scale.jaldee.com/business/"])
-def test_Rbac_Enabled_OrderInvoice(login):
+def test_Rbac_Enabled_createorderInvoice(login):
      try:
         wait_and_locate_click(login, By.XPATH, "//div[contains(text(),'Settings')]")
         time.sleep(2)
@@ -142,7 +140,7 @@ def test_Rbac_Enabled_OrderInvoice(login):
         time.sleep(2)
         wait_and_locate_click(login, By.XPATH, "//tbody/tr[2]/td[3]/div[1]/div[1]/p-button[1]/button[1]/span[1]")
         time.sleep(2)
-         salesorder_invoice = WebDriverWait(login, 10).until(
+        salesorder_invoice = WebDriverWait(login, 10).until(
             EC.presence_of_element_located((By.XPATH, "//label[normalize-space()='Sales Order Invoice']"))
             )
         scroll_to_element(login, salesorder_invoice)
@@ -156,11 +154,11 @@ def test_Rbac_Enabled_OrderInvoice(login):
         if is_checked:
             pass
         else:
-            print("Checkbox is selected, proceeding to enabled the create order catalog..")
-            create_order_catalog = WebDriverWait(login, 10).until(
+            print("Checkbox is selected, proceeding to enabled the create order Invoice..")
+            create_order_invoice = WebDriverWait(login, 10).until(
                 EC.presence_of_element_located((By.XPATH, "(//div[@class='mdc-form-field'])[12]"))
             )
-            create_order_catalog.click()
+            create_order_invoice.click()
             time.sleep(2)
             wait_and_locate_click(login, By.XPATH, "//button[normalize-space()='Save']")
             time.sleep(2)
@@ -174,8 +172,6 @@ def test_Rbac_Enabled_OrderInvoice(login):
         create_order = WebDriverWait(login, 10).until(
             EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Create Order')]"))
         )
-        assert create_order.is_displayed(), "Create_Order button is NOT displayed after enabling checkbox!"
-        print("Create_Order button is displayed. Proceeding to create order.")
         create_order.click()
         time.sleep(2)
         wait_and_send_keys(login, By.XPATH, "//input[@placeholder='Search patients']", "5550009954")
@@ -196,7 +192,16 @@ def test_Rbac_Enabled_OrderInvoice(login):
         scroll_to_element(login, confirmorder)
         time.sleep(2)
         confirmorder.click()
-        time.sleep(5)
+        time.sleep(3)
+        createorderinvoice = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "(//button[@class='p-element draft-button me-1 p-button p-component'])[1]"))
+        )
+        scroll_to_element(login, createorderinvoice)
+        time.sleep(2)
+        assert createorderinvoice.is_displayed(), "Createorder Invoice button is NOT displayed after enabling checkbox!"
+        print("Createorder Invoice button is displayed. Proceeding to create order Invoice.")
+        createorderinvoice.click()
+        time.sleep(3)
      except Exception as e:
           allure.attach(  
           login.get_screenshot_as_png(),  
@@ -204,3 +209,201 @@ def test_Rbac_Enabled_OrderInvoice(login):
           attachment_type=AttachmentType.PNG,
           )
           raise e
+     
+     
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title("RBAC Jaldee Order POS with disabling View Salesorder Invoice ")
+@pytest.mark.parametrize("url", ["https://scale.jaldee.com/business/"])
+def test_Rbac_Disabled_vieworderInvoice(login):
+     try:
+        wait_and_locate_click(login, By.XPATH, "//div[contains(text(),'Settings')]")
+        time.sleep(2)
+        rbac = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='RBAC']"))
+            )
+        scroll_to_element(login, rbac)
+        time.sleep(2)
+        rbac.click()
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//h2[normalize-space()='Roles']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//span[normalize-space()='Jaldee Order POS']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//tbody/tr[2]/td[3]/div[1]/div[1]/p-button[1]/button[1]/span[1]")
+        time.sleep(2)
+        salesorder_invoice = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//label[normalize-space()='Sales Order Invoice']"))
+            )
+        scroll_to_element(login, salesorder_invoice)
+        time.sleep(2)
+        checkbox = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//label[contains(text(), 'View Order Invoice')]/preceding-sibling::div//input"))
+        )
+        # Check if the checkbox is selected
+        is_checked = checkbox.is_selected()
+        print("Checkbox is selected:", is_checked)
+        if is_checked:
+            print("Checkbox is selected, proceeding to disabled the create order invoice ..")
+            view_order_invoice = WebDriverWait(login, 10).until(
+                EC.presence_of_element_located((By.XPATH, "(//div[@class='mdc-form-field'])[14]"))
+            )
+            view_order_invoice.click()
+            time.sleep(2)
+            wait_and_locate_click(login, By.XPATH, "//button[normalize-space()='Save']")
+            time.sleep(2)
+            wait_and_locate_click(login, By.XPATH, "//button[normalize-space()='Yes']")
+            message = get_snack_bar_message(login)
+            print("Snack bar message:", message)
+            time.sleep(2)
+        else:
+            pass
+        time.sleep(3)
+        wait_and_locate_click(login, By.XPATH, "//li[4]//a[1]//div[1]//span[1]//span[1]//img[1]")
+        time.sleep(2)
+        create_order = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Create Order')]"))
+        )
+        create_order.click()
+        time.sleep(2)
+        wait_and_send_keys(login, By.XPATH, "//input[@placeholder='Search patients']", "5550009954")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//span[normalize-space()='Id : 1']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//span[normalize-space()='Next']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//div[@class='d-flex item-btn align-items-center']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "(//div[@class='mdc-checkbox'])[2]")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//button[@class='p-element p-button-primary p-button p-component']")
+        time.sleep(2)
+        confirmorder = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='Confirm Order']"))
+        )
+        scroll_to_element(login, confirmorder)
+        time.sleep(2)
+        confirmorder.click()
+        time.sleep(3)
+        createorderinvoice = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "(//button[@class='p-element draft-button me-1 p-button p-component'])[1]"))
+        )
+        scroll_to_element(login, createorderinvoice)
+        time.sleep(2)
+        createorderinvoice.click()
+        time.sleep(2)
+        View_Invoice = WebDriverWait(login, 10).until(
+            EC.invisibility_of_element_located((By.XPATH, "//span[normalize-space()='View Invoice']"))
+        )
+        if not View_Invoice:
+            print("Testcase Failed: View_Invoice button is visible after disabling checkbox.")
+            assert False, "View_Invoice button should NOT be visible after disabling the checkbox!"
+        else:
+            print("Test passed: View_Invoice button is NOT visible after disabling checkbox.")
+        time.sleep(3)
+        
+     except Exception as e:
+          allure.attach(  
+          login.get_screenshot_as_png(),  
+          name="full_page", 
+          attachment_type=AttachmentType.PNG,
+          )
+          raise e
+     
+
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title("RBAC Jaldee Order POS with enabling View Salesorder invoice")
+@pytest.mark.parametrize("url", ["https://scale.jaldee.com/business/"])
+def test_Rbac_Enabled_ViewcreateorderInvoice(login):
+     try:
+        wait_and_locate_click(login, By.XPATH, "//div[contains(text(),'Settings')]")
+        time.sleep(2)
+        rbac = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='RBAC']"))
+            )
+        scroll_to_element(login, rbac)
+        time.sleep(2)
+        rbac.click()
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//h2[normalize-space()='Roles']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//span[normalize-space()='Jaldee Order POS']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//tbody/tr[2]/td[3]/div[1]/div[1]/p-button[1]/button[1]/span[1]")
+        time.sleep(2)
+        salesorder_invoice = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//label[normalize-space()='Sales Order Invoice']"))
+            )
+        scroll_to_element(login, salesorder_invoice)
+        time.sleep(2)
+        checkbox = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//label[contains(text(), 'View Order Invoice')]/preceding-sibling::div//input"))
+        )
+        # Check if the checkbox is selected
+        is_checked = checkbox.is_selected()
+        print("Checkbox is selected:", is_checked)
+        if is_checked:
+            pass
+        else:
+            print("Checkbox is selected, proceeding to enabled the View order Invoice..")
+            view_order_invoice = WebDriverWait(login, 10).until(
+                EC.presence_of_element_located((By.XPATH, "(//div[@class='mdc-form-field'])[14]"))
+            )
+            view_order_invoice.click()
+            time.sleep(2)
+            wait_and_locate_click(login, By.XPATH, "//button[normalize-space()='Save']")
+            time.sleep(2)
+            wait_and_locate_click(login, By.XPATH, "//button[normalize-space()='Yes']")
+            message = get_snack_bar_message(login)
+            print("Snack bar message:", message)
+            time.sleep(2)
+        time.sleep(3)
+        wait_and_locate_click(login, By.XPATH, "//li[4]//a[1]//div[1]//span[1]//span[1]//img[1]")
+        time.sleep(2)
+        create_order = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Create Order')]"))
+        )
+        create_order.click()
+        time.sleep(2)
+        wait_and_send_keys(login, By.XPATH, "//input[@placeholder='Search patients']", "5550009954")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//span[normalize-space()='Id : 1']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//span[normalize-space()='Next']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//div[@class='d-flex item-btn align-items-center']")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "(//div[@class='mdc-checkbox'])[2]")
+        time.sleep(2)
+        wait_and_locate_click(login, By.XPATH, "//button[@class='p-element p-button-primary p-button p-component']")
+        time.sleep(2)
+        confirmorder = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='Confirm Order']"))
+        )
+        scroll_to_element(login, confirmorder)
+        time.sleep(2)
+        confirmorder.click()
+        time.sleep(3)
+        createorderinvoice = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "(//button[@class='p-element draft-button me-1 p-button p-component'])[1]"))
+        )
+        scroll_to_element(login, createorderinvoice)
+        time.sleep(2)
+        createorderinvoice.click()
+        time.sleep(2)
+        Vieworderinvoice = WebDriverWait(login, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='View Invoice']"))
+        )
+        scroll_to_element(login, Vieworderinvoice)
+        time.sleep(2)
+        assert Vieworderinvoice.is_displayed(), "View order Invoice button is NOT displayed after enabling checkbox!"
+        print("View order Invoice button is displayed. Proceeding to create order Invoice.")
+        Vieworderinvoice.click()
+        time.sleep(3)
+     except Exception as e:
+          allure.attach(  
+          login.get_screenshot_as_png(),  
+          name="full_page", 
+          attachment_type=AttachmentType.PNG,
+          )
+          raise e
+     
