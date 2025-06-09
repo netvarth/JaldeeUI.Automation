daf930cf52900618acdd08b2946039eea4e24f1b
Sanoop A S <sanoop.as@jaldee.com>
2025-06-09 17:42:54 +0530

    09/06/2025 update IP management

--- Selenium Project/IP_Management/IP_Maganement_workflow.py
+++ Selenium Project/IP_Management/IP_Maganement_workflow.py
@@ -530,14 +530,146 @@ def test_IP_workflow(login):
                 (By.XPATH, "(//*[name()='svg'][@class='p-icon p-multiselect-close-icon'])[1]"))
         ).click()
 
+        time.sleep(1)
+        wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "(//*[name()='svg'][@class='p-icon'])[1]"))
+        ).click()
+
+        time.sleep(2)
+        # Locate the minute up arrow
+        minute_up_button = login.find_element(By.XPATH, "//div[contains(@class,'p-minute-picker')]//button[1]")
+
+        # Click it 10 times
+        for _ in range(10):
+            minute_up_button.click()
+            time.sleep(0.2)  # slight delay to ensure the click registers
+
+        print("✅ Increased minutes by 10 clicks.")
+        time.sleep(2)
+
+        today_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//td[contains(@class, 'p-datepicker-today')]//span")))
+
+        # Click on today's date
+        today_element.click()
+
+        print("Clicked today's date:", today_element.text)
+        
+        time.sleep(2)
+        wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "(//*[name()='svg'][@class='p-icon'])[2]"))
+        ).click()
+
+        time.sleep(2)
+        # Locate the minute up arrow
+        minute_up_button = login.find_element(By.XPATH, "//div[contains(@class,'p-minute-picker')]//button[1]")
+
+        # Click it 10 times
+        for _ in range(20):
+            minute_up_button.click()
+            time.sleep(0.2)  # slight delay to ensure the click registers
+
+        print("✅ Increased minutes by 20 clicks.")
+
+        time.sleep(2)
+
+        today_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//td[contains(@class, 'p-datepicker-today')]//span")))
+
+        # Click on today's date
+        today_element.click()
+
+        print("Clicked today's date:", today_element.text)
+
+        time.sleep(1)
+        wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "(//button[normalize-space()='Add Service'])[1]"))
+        ).click()
+
+        time.sleep(2)
+        wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "(//div[normalize-space()='Prescription'])[1]"))
+        ).click()
+
+        time.sleep(2)
+        for i in range(5):
+            login.find_element(
+                By.XPATH, "//button[normalize-space()='+ Add Medicine']"
+            ).click()
+            login.find_element(By.XPATH, "//input[@role='searchbox']").send_keys(
+                "Medicine"
+            )
+
+            # Scope to the prescription modal dialog
+            
+            before_XPath = "//mat-dialog-container[@role='dialog' and contains(@class, 'mat-mdc-dialog-container')]//table[contains(@class, 'p-datatable-table')]//tbody/tr"
+            aftertd_XPath_1 = "/td[2]"
+            aftertd_XPath_2 = "/td[3]"
+            aftertd_XPath_3 = "/td[4]"
+            aftertd_XPath_4 = "/td[5]"
+            textarea_xpath = "//input[@role='searchbox']"
+            row = i + 1
+            if i > 0:
+                trXPath = before_XPath + str([row])
+            else:
+                trXPath = before_XPath
+
+            PreFinalXPath = trXPath + aftertd_XPath_1
+            FinalXPath = PreFinalXPath + textarea_xpath
+
+            Dose = login.find_element(By.XPATH, PreFinalXPath)
+            Dose.click()
+            Dose1 = login.find_element(By.XPATH, FinalXPath)
+            Dose1.send_keys("650 mg")
+
+            PreFinalXPath = trXPath + aftertd_XPath_2
+            FinalXPath = PreFinalXPath + textarea_xpath
+
+            Frequency = login.find_element(By.XPATH, PreFinalXPath)
+            Frequency.click()
+            Frequency1 = login.find_element(By.XPATH, FinalXPath)
+            Frequency1.send_keys("1-1-1")
+
+            PreFinalXPath = trXPath + aftertd_XPath_3
+            FinalXPath = PreFinalXPath + textarea_xpath
+            Duration = login.find_element(By.XPATH, PreFinalXPath)
+            Duration.click()
+            Duration1 = login.find_element(By.XPATH, FinalXPath)
+            Duration1.send_keys("5 Days")
+
+            PreFinalXPath = trXPath + aftertd_XPath_4
+            FinalXPath = PreFinalXPath + textarea_xpath
+            Notes = login.find_element(By.XPATH, PreFinalXPath)
+            Notes.click()
+            Notes1 = login.find_element(By.XPATH, FinalXPath)
+            Notes1.send_keys("After Food")
+
+        
+        
+        try:
+
+            snack_bar = WebDriverWait(login, 10).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "snackbarnormal"))
+            )
+            message = snack_bar.text
+            print("Snack bar message:", message)
+
+        except:
+
+            snack_bar = WebDriverWait(login, 10).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "snackbarerror"))
+            )
+            message = snack_bar.text
+            print("Snack bar message:", message)
+
+
+        
         
 
+        
         time.sleep(10)
- 
-
-
-
-
     except Exception as e:
         allure.attach(  # use Allure package, .attach() method, pass 3 params
             login.get_screenshot_as_png(),  # param1
