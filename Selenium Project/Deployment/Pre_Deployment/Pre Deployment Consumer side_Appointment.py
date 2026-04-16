f702861856a8bc30be31cb6dade56d18bb9d0ea2
Jisha <jisha.rajan@jaldee.com>
2026-04-16 15:45:53 +0530

    predeployment consumer side update 16.04.26

--- Selenium Project/Deployment/Pre_Deployment/Pre Deployment Consumer side_Appointment.py
+++ Selenium Project/Deployment/Pre_Deployment/Pre Deployment Consumer side_Appointment.py
@@ -2,17 +2,7 @@ import time
 from datetime import datetime, timedelta
 from Framework.consumer_common_utils import *
 
-import pyautogui
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-import os
-import allure
-from allure_commons.types import AttachmentType
+
 
 
 @allure.severity(allure.severity_level.CRITICAL)
